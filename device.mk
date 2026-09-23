51979cbb84af82deacae7339930b7b1969c1bdf5
szyryjn911 <amarkumarray47@gmail.com>
2026-09-23 17:20:15 +0000

    RMX2020: Update included soong namespace

--- device.mk
+++ device.mk
@@ -331,7 +331,9 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
-    hardware/google/pixel \
+    hardware/google/pixel/pixelstats \
+    hardware/google/pixel/power-libperfmgr \
+    hardware/google/pixel/thermal \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
     hardware/mediatek/wlan/wifi_hal \
